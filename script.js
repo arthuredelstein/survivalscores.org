6907fe27f82624dfad94e5a6107fd23f031c01e1
Arthur Edelstein <arthuredelstein@gmail.com>
2022-11-16 17:54:40 -0800

    smarter sorting

--- script.js
+++ script.js
@@ -2,20 +2,21 @@
 // Initially will be sorted by population
 let state = { sortedColumn: 1, ascending: false };
 
-const rowComparator = (row1, row2, columnIndex) => {
+const rowComparator = (row1, row2, columnIndex, reverse = false) => {
+  const reverseFactor = reverse ? -1 : 1;
   const td1 = [...row1.querySelectorAll("td")][columnIndex];
   const td2 = [...row2.querySelectorAll("td")][columnIndex];
   const text1 = td1.innerText.trim();
   const text2 = td2.innerText.trim();
   if (text1.length > 0 && !isNaN(text1)) {
-    return parseInt(text1) - parseInt(text2);
+    return reverseFactor * (parseInt(text1) - parseInt(text2));
   }
   const class1 = td1.className;
   const class2 = td2.className;
   if (class1 && (class1.includes("good") || class1.includes("bad"))) {
-    return class1.localeCompare(class2);
+    return reverseFactor * class1.localeCompare(class2);
   }
-  return text1.localeCompare(text2);
+  return reverseFactor * text1.localeCompare(text2);
 };
 
 const sortValue = (row, columnIndex) => {
@@ -32,10 +33,7 @@ const sortColumn = (columnIndex) => {
   state.sortedColumn = columnIndex;
   const rows = [...document.querySelectorAll("table tr")].slice(1);
   const table = document.querySelector("table");
-  let rows2 = rows.sort((rowA, rowB) => rowComparator(rowA, rowB, columnIndex));
-  if (state.ascending === false) {
-    rows2 = rows2.reverse();
-  }
+  let rows2 = rows.sort((rowA, rowB) => rowComparator(rowA, rowB, columnIndex, state.ascending));
   for (const row of rows2) {
     table.appendChild(row);
   }
