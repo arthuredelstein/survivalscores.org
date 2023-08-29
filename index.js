2b7927003e1a29148e1c4edea84aedcf20e31e0c
Arthur Edelstein <arthuredelstein@gmail.com>
2023-08-29 14:16:48 -0700

    beging migrating to new graphql data stuff

--- index.js
+++ index.js
@@ -142,9 +142,134 @@ const tableRowsFromUrl = async (url, selector) => {
   return [...table.querySelectorAll("tr")];
 };
 
+const disarmamentGraphQLQuery = {
+  operationName: 'Treaty',
+  variables: { input: { id: null, short_name: 'tpnw', type: 'GET_TREATY' } },
+  query: `query Treaty($input: TreatyRequestInput_) {
+    treaty_(input: $input) {
+      ... on ReadResponse {
+        data {
+          ... on Treaty_ {
+            id
+            description
+            full_name
+            is_protocol
+            name
+            protocol_parent_id
+            protocols_ {
+              id
+              name
+              short_name
+              count_states_parties
+              adopted_date
+              open_for_signature_date
+              __typename
+            }
+            short_name
+            treaty_text
+            note
+            is_amendment
+            signature_location
+            show_deposit_location
+            show_signature_location
+            show_signature_for_protocol
+            adopted_date
+            adopted_location
+            open_for_signature_date
+            entry_into_force_date
+            entry_into_force_note
+            search_keywords
+            certified_copy_url
+            count_sig_states
+            count_states_parties
+            depositaries_ {
+              id
+              short_name
+              name
+              url_name
+              __typename
+            }
+            actions_ {
+              id
+              date
+              note
+              type
+              action_type_
+              state {
+                name
+                country_id
+                country_ {
+                  country_persistent_name
+                  country_country_name {
+                    countryname_official_short_name
+                    countryname_official_long_name
+                    countryname_country_code_2
+                    __typename
+                  }
+                  country_country_groups {
+                    countrygroup_group_name
+                    countrygroup_group_type
+                    __typename
+                  }
+                  __typename
+                }
+                __typename
+              }
+              depositary {
+                id
+                short_name
+                name
+                url_name
+                __typename
+              }
+              objections_ {
+                date
+                note
+                state {
+                  country_ {
+                    country_persistent_name
+                    country_country_name {
+                      countryname_country_code_3
+                      countryname_official_short_name
+                      __typename
+                    }
+                    __typename
+                  }
+                  __typename
+                }
+                __typename
+              }
+              __typename
+            }
+            __typename
+          }
+          __typename
+        }
+        __typename
+      }
+      __typename
+    }
+  }`
+};
+
+const getDisarmamentTreatyData = async (treaty) => {
+  const response = await fetch("https://gql-api-dataportal.unoda.org/", {
+    "headers": {
+      "content-type": "application/json",
+    },
+    "body": JSON.stringify(disarmamentGraphQLQuery),
+    "method": "POST",
+  });
+  const json = await response.json();
+  return json.data.treaty_.data;
+}
+
 const disarmamentTreatyInfo = async (treaty, tableSelector) => {
-  const rows = await tableRowsFromUrl(`https://treaties.unoda.org/t/${treaty}`, tableSelector);
-  return rows.slice(1).map(getDisarmamentDataFromRow);
+  const dom = await domFromUrl(`https://treaties.unoda.org/t/${treaty}/participants`);
+  const table = dom.window.document.querySelectorAll("mat-table");
+  return table;
+  //return table.querySelectorAll("mat-row")
+//  return rows.slice(1).map(getDisarmamentDataFromRow);
 };
 
 const unTreatyInfo = async (url, columnCount) => {
