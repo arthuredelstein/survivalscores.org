f817aa40857d7377dd7aba141dce78929b65725b
Arthur Edelstein <arthuredelstein@gmail.com>
2024-01-14 19:54:58 -0800

    add timeline to build

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { collectAllData } from './collect.js'
 import { renderTable, aboutPage } from './render-table.js'
+import { renderTimelinePage } from './render-timeline.js'
 import esMain from 'es-main'
 
 const main = async () => {
   const dataDate = new Date()
   const { aggregatedData, populationData } = await collectAllData()
   await renderTable({ aggregatedData, populationData, dataDate }, true)
+  await renderTimelinePage({ aggregatedData, dataDate }, true)
   await aboutPage(dataDate)
 }
 
